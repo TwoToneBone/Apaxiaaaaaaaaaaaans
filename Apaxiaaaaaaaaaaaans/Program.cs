24bd16fa5115b39f2712ba51a541d0df54656943
Erik <erik.ra@gmail.com>
2017-04-26 23:03:25 +0200

    Add project files.

---
+++ Apaxiaaaaaaaaaaaans/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apaxiaaaaaaaaaaaans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string output = string.Empty;
+            char comparer = ' ';
+
+            foreach (var c in input)
+            {
+                if (c == comparer)
+                {
+                    continue;
+                }
+                else
+                {
+                    comparer = c;
+                    output += c;
+                }
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
